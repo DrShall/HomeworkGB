386d08e77bd9974ac7435957d0e943c2bdb990bd
DrShall <shalagindk@yandex.ru>
2021-12-11 17:56:56 +0500

    DZ 3

---
+++ Algorithms and data structures/DZ3/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int);
+
+int main()
+{
+	int num;
+	bool flag;
+
+	cout << "Enter number (more then 1): ";
+	cin >> num;
+
+	if(num < 2)   //если число меньше двух не можем говорить о простоте числа
+	{
+		cout << "Incorrect input" << endl;
+		return 0;
+	}
+
+	flag = isPrime(num);
+
+	if(flag)
+		cout << num << " is prime number" << endl;
+	else
+		cout << num << " is NOT prime number" << endl;
+
+	return 0;
+}
+
+bool isPrime(int n)
+{
+	if( n < 4)   //2 и 3 простые числа
+		return true;
+
+	if( n % 2 == 0)   //четные числа не являются простыми
+	{
+		return false;
+	}
+
+	for( int i = 3; i * i <= n; i+=2)   //перебираем нечетные делители до корень из n
+	{
+		if( n % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
