ef1eeee7788d983398dcff129f799534bb77d2b8
DrShall <shalagindk@yandex.ru>
2022-01-20 04:19:49 +0500

    DZ 16

--- Algorithms and data structures/DZ16/main.cpp
+++ Algorithms and data structures/DZ16/main.cpp
@@ -6,14 +6,18 @@
 //содержащий функцию main и все необходимые функции.
 
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 void cesarEncryption(char*,int,int);
 void cesarDecryption(char*,int,int);
+void reshuffleEncryption(char*,int,int);
+void reshuffleDecryption(char*,int,int);
 
 int main()
 {
+	srand(time(NULL));
 	// Task 1
 	const int number = 256;   //длина строки
 	char* str = new char[number]{' '};
@@ -22,14 +26,25 @@ int main()
 	cout << endl << "Enter Cesar encryption key: ";
 	int key;
 	cin >> key;
+	int len = 0;
+	for(int i = 0; i < number; ++i)
+	{
+		if(str[i] != '\0')
+			len++;
+		else
+			break;
+	}
 
-	cesarEncryption(str, number, key);
+	cesarEncryption(str, len, key);
 	cout << endl << "Encryption string: " << str << endl;
-	cesarDecryption(str, number, key);
+	cesarDecryption(str, len, key);
 	cout << endl << "Decryption string: " << str << endl;
 
 	// Task 2
-
+	reshuffleEncryption(str, len, key);
+	cout << endl << "Encryption string: " << str << endl;
+	reshuffleDecryption(str, len, key);
+	cout << endl << "Decryption string: " << str << endl;
 
 	delete[] str;
 
@@ -41,6 +56,8 @@ void cesarEncryption(char* str, int num, int key)
 	int t;
 	for(int i = 0; i < num; ++i)
 	{
+		if(str[i] == '\0')
+			return;
 		t = str[i];
 		if(t > 64 && t < 91)   //если большие латинские буквы
 		{
@@ -59,6 +76,8 @@ void cesarDecryption(char* str, int num, int key)
 	int t;
 	for(int i = 0; i < num; ++i)
 	{
+		if(str[i] == '\0')
+			return;
 		t = str[i];
 		if(t > 64 && t < 91)   //если большие латинские буквы
 		{
@@ -72,4 +91,56 @@ void cesarDecryption(char* str, int num, int key)
 	}
 }
 
+void reshuffleEncryption(char* str, int num, int col)
+{
+	int row = num / col;
+	if(num % col != 0)
+		row++;
+	char* tmp = new char[row*col];
 
+	int t = 0;
+	for(int i = 0; i < col; ++i)
+	{
+		for(int j = i; j < row*col; j += col)
+		{
+			if(j < num)
+				tmp[t++] = str[j];
+			else
+				tmp[t++] = 97 + rand()%26;
+		}
+	}
+	t=0;
+	for(int i = 0; i < row; ++i)
+		for(int j = 0; j < col; ++j)
+		{
+			str[t++] = tmp[i*col+j];
+		}
+
+	delete[] tmp;
+}
+
+void reshuffleDecryption(char* str, int num, int col)
+{
+	int row = num / col;
+	if(num % col != 0)
+		row++;
+	char* tmp = new char[row*col];
+
+	int t = 0;
+	for(int i = 0; i < col; ++i)
+	{
+		for(int j = i; j < row*col; j += col)
+		{
+				tmp[j] = str[t++];
+		}
+	}
+	t=0;
+	for(int i = 0; i < row; ++i)
+		for(int j = 0; j < col; ++j)
+		{
+			if(i*col+j < num)
+				str[t++] = tmp[i*col+j];
+			else
+				str[t++] = '\0';
+		}
+}
