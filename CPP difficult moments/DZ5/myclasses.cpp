8697a23976aba3a7f91d4cc156531704bbc06d8b
DrShall <shalagindk@yandex.ru>
2022-05-21 15:49:26 +0500

    DZ 5

--- CPP difficult moments/DZ5/myclasses.cpp
+++ CPP difficult moments/DZ5/myclasses.cpp
@@ -1,11 +1,18 @@
 #include "myclasses.h"
 
+bool cmp (std::pair<std::string,int>& p1, std::pair<std::string,int>& p2)
+{
+	if(p1.second == p2.second)
+		return p1.first < p2.first;
+	else
+		return p1.second < p2.second;
+}
+
 void countSentence( std::string& str)
 {
 	const std::string punctuationMarks { ".?!"};
-	std::set< std::pair<std::string, int> > sentences;
+	std::set< std::pair<std::string, int>, decltype(cmp)* > sentences(&cmp);
 	std::string tempStr;
-//	std::pair<std::string, int> p;
 	int count_c = 1;
 
 	for( const char& c : str)
@@ -23,17 +30,17 @@ void countSentence( std::string& str)
 			{
 				count_c++;
 			}
+			else if(c == ' ' && tempStr.empty())
+				continue;
 			tempStr +=c;
 		}
 
 	}
 
-	sort( sentences.begin(), sentences.end(), [](std::pair<std::string,int>& p1, std::pair<std::string,int>& p2){
-		return p1.second < p2.second;
-	});
+//	std::sort( sentences.begin(), sentences.end(), [](std::pair<std::string,int>& p1, std::pair<std::string,int>& p2){ return p1.second < p2.second; });
 	for( auto [str, c] : sentences)
 	{
-		std::cout << str << " " << c << std::endl;
+		std::cout << str << "  - " << c << std::endl;
 	}
 }
 
