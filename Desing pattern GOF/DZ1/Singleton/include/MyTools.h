f311ffd4debf93a478561f24f1869f216350c2df
DrShall <shalagindk@yandex.ru>
2022-08-27 11:55:44 +0500

    Singleton

---
+++ Desing pattern GOF/DZ1/Singleton/include/MyTools.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+#include <cstring>
+
+/*namespace MyTools {
+
+void OpenLogFile(const std::string& FN);
+
+void CloseLogFile();
+
+void WriteToLog(const std::string& str);
+
+void WriteToLog(const std::string& str, int n);
+
+void WriteToLog(const std::string& str, double d);
+
+}; */// namespace MyTools
+
+class FileLoggerSingleton
+{
+private:
+
+	FileLoggerSingleton() {}
+	~FileLoggerSingleton() {}
+	FileLoggerSingleton(const FileLoggerSingleton& root) = delete;
+	FileLoggerSingleton& operator=(const FileLoggerSingleton&) = delete;
+	std::string GetCurDateTime();
+
+public:
+	static FileLoggerSingleton& getInstance()
+	{
+		static FileLoggerSingleton instance;
+		return instance;
+	}
+
+	void  OpenLogFile(const std::string& FN);
+	void CloseLogFile();
+	void  WriteToLog(const std::string& str);
+	void  WriteToLog(const std::string& str, int n);
+	void  WriteToLog(const std::string& str, double d);
+};
---
+++ Desing pattern GOF/DZ1/Singleton/src/MyTools.cpp
@@ -0,0 +1,86 @@
+#include "MyTools.h"
+
+#include <stdint.h>
+#include <time.h>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <stdio.h>
+
+std::ofstream logOut;
+
+void FileLoggerSingleton::OpenLogFile(const std::string& FN)
+{
+	logOut.open(FN, std::ios_base::out);
+}
+void FileLoggerSingleton::CloseLogFile()
+{
+	if (logOut.is_open()) {
+		logOut.close();
+	  }
+}
+void FileLoggerSingleton::WriteToLog(const std::string& str)
+{
+	if (logOut.is_open()) {
+		logOut << GetCurDateTime() << " - " << str << std::endl;
+	  }
+}
+void FileLoggerSingleton::WriteToLog(const std::string& str, int n)
+{
+	if (logOut.is_open()) {
+		logOut << GetCurDateTime() << " - " << str << n << std::endl;
+	  }
+}
+void FileLoggerSingleton::WriteToLog(const std::string& str, double d)
+{
+	if (logOut.is_open()) {
+		logOut << GetCurDateTime() << " - " << str << d << std::endl;
+	  }
+}
+std::string FileLoggerSingleton::GetCurDateTime() {
+  auto cur = std::chrono::system_clock::now();
+  time_t time = std::chrono::system_clock::to_time_t(cur);
+  char* buf = ctime(&time);
+  return std::string(buf);
+}
+
+namespace MyTools {
+
+std::ofstream logOut;
+
+void OpenLogFile(const std::string &FN) { logOut.open(FN, std::ios_base::out); }
+
+void CloseLogFile() {
+  if (logOut.is_open()) {
+	logOut.close();
+  }
+}
+
+std::string GetCurDateTime() {
+  auto cur = std::chrono::system_clock::now();
+  time_t time = std::chrono::system_clock::to_time_t(cur);
+  char* buf = ctime(&time);
+  return std::string(buf);
+}
+
+void WriteToLog(const std::string &str) {
+  if (logOut.is_open()) {
+	logOut << GetCurDateTime() << " - " << str << std::endl;
+  }
+}
+
+void WriteToLog(const std::string &str, int n) {
+  if (logOut.is_open()) {
+	logOut << GetCurDateTime() << " - " << str << n << std::endl;
+  }
+}
+
+void WriteToLog(const std::string &str, double d) {
+  if (logOut.is_open()) {
+	logOut << GetCurDateTime() << " - " << str << d << std::endl;
+  }
+}
+
+} // namespace MyTools
